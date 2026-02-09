456c1792f51fb318a8b5d405713d59de1a451613
Devansh Soni <devanshsoni899@gmail.com>
2026-02-09 19:09:10 +0530

    fix: permission error issue in drafting mail

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,8 @@ REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
 SCOPES = [
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/gmail.send',
+    'https://www.googleapis.com/auth/gmail.compose',
+    'https://www.googleapis.com/auth/gmail.modify',
     'https://www.googleapis.com/auth/cloud-platform'
 ]
 
